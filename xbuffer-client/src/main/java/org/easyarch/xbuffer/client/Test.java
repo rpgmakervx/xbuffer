d8e188274d348afc6159aa6808b08df3d64d2b48
xingtianyu <583110127@qq.com>
2018-10-20 23:02:37 +0800

    init

---
+++ xbuffer-client/src/main/java/org/easyarch/xbuffer/client/Test.java
@@ -0,0 +1,7 @@
+package org.easyarch.xbuffer.client;
+
+/**
+ * Created by xingtianyu on 2018/10/20.
+ */
+public class Test {
+}
---
+++ xbuffer-kernel/src/main/java/org/easyarch/xbuffer/kernel/Test.java
@@ -0,0 +1,7 @@
+package org.easyarch.xbuffer.kernel;
+
+/**
+ * Created by xingtianyu on 2018/10/20.
+ */
+public class Test {
+}
