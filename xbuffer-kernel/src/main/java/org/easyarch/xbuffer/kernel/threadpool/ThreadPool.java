81a7728ff80be544c71f4b4eed48cb0feb11c8df
xingtianyu <583110127@qq.com>
2018-11-30 01:30:05 +0800

    setting封装到guice的module中。设置Environment到settings中。
    TODO:线程模型

--- xbuffer-kernel/src/main/java/org/easyarch/xbuffer/kernel/threadpool/ThreadPool.java
+++ xbuffer-kernel/src/main/java/org/easyarch/xbuffer/kernel/threadpool/ThreadPool.java
@@ -1,7 +1,18 @@
 package org.easyarch.xbuffer.kernel.threadpool;
 
+import org.easyarch.xbuffer.kernel.common.component.AbstractComponent;
+import org.easyarch.xbuffer.kernel.env.Settings;
+
 /**
  * Created by xingtianyu on 2018/11/30.
  */
-public class ThreadPool {
+public class ThreadPool extends AbstractComponent{
+
+    public ThreadPool(Settings settings) {
+        super(settings);
+    }
+
+    public class ThreadState{
+
+    }
 }
---
+++ xbuffer-kernel/src/main/java/org/easyarch/xbuffer/kernel/threadpool/ThreadPoolModule.java
@@ -0,0 +1,14 @@
+package org.easyarch.xbuffer.kernel.threadpool;
+
+import com.google.inject.AbstractModule;
+import org.easyarch.xbuffer.kernel.common.component.AbstractComponent;
+
+/**
+ * Created by xingtianyu on 2018/11/30.
+ */
+public class ThreadPoolModule extends AbstractModule {
+    @Override
+    protected void configure() {
+
+    }
+}
